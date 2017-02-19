8a271e9c5769ed90c23d60637dd3c484ee147851
ek <ekkapob@gmail.com>
2017-02-19 22:14:09 +0700

    setup templates

--- handler/global.go
+++ handler/global.go
@@ -2,13 +2,26 @@ package handler
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "123test")
+	t := template.Must(baseTemplate("home.tmpl"))
+	t.ExecuteTemplate(w, "base", nil)
 }
 
 func NotFound(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "not found")
+}
+
+func baseTemplate(contentTmpl string) (*template.Template, error) {
+	return template.ParseFiles(
+		"templates/header.tmpl",
+		"templates/base.tmpl",
+		"templates/footer.tmpl",
+		"templates/scripts.tmpl",
+		"templates/styles.tmpl",
+		fmt.Sprintf("templates/%v", contentTmpl),
+	)
 }
