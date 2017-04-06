44c6ac2ecefe1f725173d47530bc5c5671956315
ek <ekkapob@gmail.com>
2017-04-06 22:18:31 +0700

    update link

--- handler/page/sale.go
+++ handler/page/sale.go
@@ -3,5 +3,5 @@ package page
 import "net/http"
 
 func Sale(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "/products?gender%5B%5D=M&gender%5B%5D=W&section%5B%5D=sale", http.StatusMovedPermanently)
+	http.Redirect(w, r, "/products?gender%5B%5D=M&gender%5B%5D=W&sections%5B%5D=sale", http.StatusMovedPermanently)
 }
